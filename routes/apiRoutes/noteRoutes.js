6813dbb01bf8fb008e55aaf83f5a7bb2c7db4d49
noreikam <noreikam1@gmail.com>
2022-05-28 20:42:38 -0400

    built notes library, POST route, GET route for all notes

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,25 @@
+const { createNote, validateNote } = require('../../lib/notes');
+const { notes } = require('../../db/db.json');
+const router = require('express').Router();
+const uniqid = require('uniqid');
+
+router.get('/notes', (req, res) => {
+  let results = notes;
+  // if (req.query) {
+  //   results = filterByQuery(req.query, results);
+  // }
+  res.json(results);
+});
+
+router.post('/notes', (req, res) => {
+  req.body.id = uniqid();
+
+  if (!validateNote(req.body)) {
+    res.status(400).send('Note not formatted properly');
+  } else {
+    const note = createNote(req.body, notes);
+    res.json(note);
+  }
+});
+
+module.exports = router;
